d6bcbf663f576d70d94ad6b340a3987e59e32db1
krizzz04 <cryptonic2003@gmail.com>
2024-05-09 23:19:08 +0530

    first online commit

---
+++ model.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose')
+const User=mongoose.model('students',
+{
+    username:{
+        type:String,unique:true,required:true
+    },
+    email:{
+        type:String,unique:true,required:true
+    },
+    password:{
+        type:String,required:true
+    },
+    aimlMarks: { type: Number, default: 0 }, // Assuming AIML marks are stored as numbers
+     dbmsMarks: { type: Number, default: 0 },
+     atcdMarks: { type: Number, default: 0 },
+     cnMarks: { type: Number, default: 0 },
+     dbmsMarks: { type: Number, default: 0 }
+
+    
+})
+
+module.exports=User
